f648074b8312acfd9194c9a9dcb6742813e20ae5
ileoni <higorleonii@gmail.com>
2025-10-17 19:28:18 -0300

    updating auth header

--- frontend/src/components/auth/Header.tsx
+++ frontend/src/components/auth/Header.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 function Header({ children }: PropsWithChildren) {
     return (
-        <header className="w-full h-16 fixed bg-white dark:bg-zinc-800 border-b border-b-zinc-200 dark:border-b-zinc-700">
+        <header className="w-full h-16 px-4 lg:px-0 fixed bg-white dark:bg-zinc-800 border-b border-b-zinc-200 dark:border-b-zinc-700">
             <div className="max-w-5xl h-full mx-auto">
                 { children }
             </div>
