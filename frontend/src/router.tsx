fa5e1053f9a156084f464c7930c836abbd7ee5b3
ileoni <higorleonii@gmail.com>
2025-10-17 15:46:10 -0300

    updating router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,32 @@
 import { createBrowserRouter } from "react-router";
+
 import App from "./App";
+import AuthLoyout from "./layouts/AuthLayout";
+import AdminLoyout from "./layouts/AdminLayout";
 
 export const router = createBrowserRouter([
     {
         Component: App,
-        index: true
+        children: [
+            {
+                index: true
+            },
+            {
+                path: "administrador",
+                children: [
+                    {
+                        Component: AuthLoyout,
+                        children: [
+                            {
+                                index: true
+                            }
+                        ]
+                    },
+                    {
+                        Component: AdminLoyout
+                    }
+                ]
+            }
+        ]
     }
 ]);
