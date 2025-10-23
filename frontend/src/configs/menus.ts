73a110f5322d7175a357ddfd0e899c1360b3fc16
ileoni <higorleonii@gmail.com>
2025-10-23 17:34:25 -0300

    updating menus

--- frontend/src/configs/menus.ts
+++ frontend/src/configs/menus.ts
@@ -26,6 +26,71 @@ export default {
                 slug: "redefinir-senha"
             }),
         },
-        authenticate: {}
+        authenticate: {
+            dashboard: setAttributes({
+                title: "painel",
+                slug: "painel",
+                isVisible: true,
+            }),
+            pages: setAttributes({
+                title: "páginas",
+                slug: "paginas",
+                isVisible: true,
+            }),
+            gallery: setAttributes({
+                title: "galeria",
+                slug: "galeria",
+                isVisible: true,
+                children: {
+                    list: setAttributes({
+                        title: "lista",
+                        slug: "galeria/lista"
+                    }),
+                    categories: setAttributes({
+                        title: "categorias",
+                        slug: "galeria/categorias"
+                    }),
+                }
+            }),
+            product: setAttributes({
+                title: "produtos",
+                slug: "produtos",
+                isVisible: true,
+                children: {
+                    list: setAttributes({
+                        title: "lista",
+                        slug: "produtos/lista"
+                    }),
+                    categories: setAttributes({
+                        title: "categorias",
+                        slug: "produtos/categorias"
+                    }),
+                    cupons: setAttributes({
+                        title: "coupons",
+                        slug: "produtos/coupons"
+                    }),
+                }
+            }),
+            orders: setAttributes({
+                title: "pedidos",
+                slug: "pedidos",
+                isVisible: true,
+            }),
+            users: setAttributes({
+                title: "usuários",
+                slug: "usuarios",
+                isVisible: true,
+            }),
+            settings: setAttributes({
+                title: "configurações",
+                slug: "configuracao",
+                isVisible: true,
+            }),
+            profile: setAttributes({
+                title: "Perfil de administrador",
+                slug: "perfil",
+                isVisible: false,
+            }),
+        }
     }
 }
