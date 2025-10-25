840fbbe09daf646d71b85d9b65ae54d4f514ed89
ileoni <higorleonii@gmail.com>
2025-10-24 23:17:24 -0300

    setting the user state
    call to the get user enpoint

--- frontend/src/components/admin/DropdownWithAvatar.tsx
+++ frontend/src/components/admin/DropdownWithAvatar.tsx
@@ -5,17 +5,32 @@ import { useConfigs } from "../../hooks/useConfigs";
 import Avatar from "../ui/Avatar";
 import Dropdown from "../ui/Dropdown";
 import SmallSizeAvatar from "../ui/HOC/SmallSizeAvatar";
+import { useEffect, useState } from "react";
+import { getUser } from "../../services/admin/user";
+
+type Image = { path: string }
+type Customer = { id: number, userId: number, firstname: string, lastname: string, bio: string, phone: string }
+type User = { id: number, email: string, password: string, role: string, customer: Customer, image: Image }
 
 function DropdownWithAvatar() {
-    const { profile, logout } = useConfigs("menus.admin.authenticate");
+    const [user, setUser] = useState<User | null>(null);
     const { auth } = useAuth();
+    const { profile, logout } = useConfigs("menus.admin.authenticate");
+
+    useEffect(() => {
+        getUser(auth.id)
+        .then(async res => {
+            const { data } = await res.json();
+            setUser(data.user);
+        })
+    }, [auth])
 
     const SmallAvatar = SmallSizeAvatar(Avatar);
 
     return (
         <Dropdown>
             <Dropdown.Toggle>
-                <SmallAvatar src={auth.images.path}/>
+                {user && (<SmallAvatar src={user.image.path}/>)}
             </Dropdown.Toggle>
             <Dropdown.Menu className="w-56! absolute right-0 rounded">
                 <ul>
