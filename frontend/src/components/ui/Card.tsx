9e6c3cf36c0e9feee47ccf9b890d6c99044fd115
ileoni <higorleonii@gmail.com>
2025-10-30 15:17:54 -0300

    adding a new card component with ref

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,11 +1,21 @@
-import type { PropsWithChildren } from "react";
+import { forwardRef, type ForwardedRef, type HTMLAttributes, type PropsWithChildren } from "react";
 
-function Card({ children }: PropsWithChildren) {
+function Card({ children, className }: HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className="h-full px-5 py-3 bg-white dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 rounded">
+        <div className={`h-full px-5 py-3 bg-white dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 rounded ${ className }`}>
             { children }
         </div>
     )
 }
+
+export const withRef = forwardRef(({ children, className }: PropsWithChildren & HTMLAttributes<HTMLDivElement>, ref: ForwardedRef<HTMLDivElement>) => {
+    return (
+        <div ref={ref} className={`h-full px-5 py-3 bg-white dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 rounded ${className}`}>
+            { children }
+        </div>
+    )
+})
+
+Card.withRef = withRef;
 
 export default Card;
