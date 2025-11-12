5a857e2ad0f65f15534171f80e5841cf00af175e
ileoni <higorleonii@gmail.com>
2025-11-12 10:50:54 -0300

    adding select element

--- frontend/src/components/ui/Label.tsx
+++ frontend/src/components/ui/Label.tsx
@@ -7,7 +7,7 @@ function Label(props: { filled?: boolean, id?: string, isDirty?: boolean, label?
         <label htmlFor={id} className={`group relative grid border border-zinc-200 dark:border-zinc-700 ${ message ? "border-red-400/50! dark:border-red-500/50!" : "" } rounded`}>
             { children }
             <div
-                className={`grid group-has-[input]:items-center absolute inset-0 group-has-[textarea]:top-6 left-5 right-auto group-has-[input:focus,textarea:focus]:-translate-y-5 ${ isDirty ? "-translate-y-5 text-xs" : ""} ${ filled ? "-translate-y-5 text-xs" : ""} group-has-[input:focus,textarea:focus]:text-xs transition-[transform_font-size]`}
+                className={`grid group-has-[input,select]:items-center absolute inset-0 group-has-[textarea]:top-6 left-5 right-auto group-has-[input:focus,textarea:focus,select:focus]:-translate-y-5 ${ isDirty ? "-translate-y-5 text-xs" : ""} ${ filled ? "-translate-y-5 text-xs" : ""} group-has-[input:focus,textarea:focus,select:focus]:text-xs transition-[transform_font-size]`}
             >
                 <span className="first-letter:capitalize">{ label }</span>
             </div>
